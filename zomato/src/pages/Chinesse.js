9a737d15c5794b3f4c3165de8bf39b126f88ee5b
neelgohelh <neelgohel8887@gmail.com>
2024-09-25 20:15:33 +0530

    25-09-2024

--- zomato/src/pages/Chinesse.js
+++ zomato/src/pages/Chinesse.js
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function Chinese(){
-    
+    const [data, setData] = useState([]);
+
+    const apiUrl = "https://62d6c51451e6e8f06f12bd5d.mockapi.io/faculties";
+
+    useEffect(()=>{
+        fetch(apiUrl, {method:"GET"})
+        .then(res=>res.json())
+        .then(res=>setData(res));
+    },[]);
+
+    const Gujrati = data.map((guj)=>{
+        return(
+            <tr>
+                <td>{guj.id}</td>
+                <td>{guj.FacultyID}</td>
+                <td>{guj.FacultyName}</td>
+                <td><Link className="btn btn-primary">Add to Cart</Link></td>
+            </tr>
+        );
+    })
+
+    return(<table className="table">{Gujrati}</table>);
 }
 
 export default Chinese;
--- zomato/src/pages/Gujrati.js
+++ zomato/src/pages/Gujrati.js
@@ -1,5 +1,45 @@
-function Gujrati(){
-    
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function Gujrati() {
+    const [data, setData] = useState([]);
+
+    const apiUrl = "https://62d6c51451e6e8f06f12bd5d.mockapi.io/faculties";
+
+    useEffect(() => {
+        fetch(apiUrl, { method: "GET" })
+            .then(res => res.json())
+            .then(res => setData(res));
+    }, []);
+
+    const Gujrati = data.map((guj) => {
+        return (
+            <div className="row">
+
+                <div class="col-3 my-2">
+                    <div class="card">
+                        <img src={guj.FacultyImage} style={{ "width": "200px" }} className="card-img-top"/>
+                        <div class="card-body">
+                            <h5 class="card-title">{guj.FacultyName}</h5>
+                            <p class="card-text">{guj.FacultyID}</p>
+                            <Link className="btn btn-primary" >Add to Cart</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    })
+
+    return ([Gujrati]);
 }
 
 export default Gujrati;
+
+
+//     <tr>
+//     <td>{guj.id}</td>
+//     <td>{guj.FacultyID}</td>
+//     <td>{guj.FacultyName}</td>
+//     <td><Link className="btn btn-primary" >Add to Cart</Link></td>
+// </tr>
